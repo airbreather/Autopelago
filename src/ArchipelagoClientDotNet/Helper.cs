f98dcd471d79967d1a511eafc6528fa738614459
Joe Amenta <airbreather@linux.com>
2024-01-12 06:02:07 -0500

    there's actually still a good reason to implement this ourselves.

--- src/ArchipelagoClientDotNet/Helper.cs
+++ src/ArchipelagoClientDotNet/Helper.cs
@@ -9,5 +9,28 @@ public static class Helper
     public static void Throw(this Exception[] exceptions) => throw new AggregateException(exceptions);
     public static void Throw(this Exception[] exceptions, string? message) => throw new AggregateException(message, exceptions);
 
-    public static ConfiguredTaskAwaitable ConfigureAwaitFalse() => Task.CompletedTask.ConfigureAwait(ConfigureAwaitOptions.ForceYielding);
+    public static GetOffSyncContextAwaitableAndAwaiter ConfigureAwaitFalse() => default;
+
+    public readonly struct GetOffSyncContextAwaitableAndAwaiter : INotifyCompletion
+    {
+        public GetOffSyncContextAwaitableAndAwaiter GetAwaiter() => default;
+
+        public bool IsCompleted => SynchronizationContext.Current is null;
+
+        public void GetResult() { }
+
+        public void OnCompleted(Action continuation)
+        {
+            SynchronizationContext? suppressed = SynchronizationContext.Current;
+            try
+            {
+                SynchronizationContext.SetSynchronizationContext(null);
+                continuation();
+            }
+            finally
+            {
+                SynchronizationContext.SetSynchronizationContext(suppressed);
+            }
+        }
+    }
 }
