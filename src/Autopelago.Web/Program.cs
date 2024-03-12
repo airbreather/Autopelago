80ab28a5d66272f91fb8e941b41045f2435074d8
Joe Amenta <airbreather@linux.com>
2024-03-11 22:08:21 -0400

    hey it's a crappy thing that actually auto-updates

--- src/Autopelago.Web/Program.cs
+++ src/Autopelago.Web/Program.cs
@@ -1,11 +1,14 @@
+using System.Net.Mime;
 using System.Net.WebSockets;
+using System.Text;
 using System.Text.Json;
-using System.Text.RegularExpressions;
 
 using ArchipelagoClientDotNet;
 
 using Autopelago;
 using Autopelago.Web;
+
+using Microsoft.AspNetCore.Mvc;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -14,16 +17,19 @@ builder.Services.AddSingleton(TimeProvider.System);
 
 builder.Services.AddHostedService<AutopelagoGameService>();
 
+builder.Services.AddControllers();
+
 WebApplication app = builder.Build();
 
 app.UseWebSockets();
+
+app.MapControllers();
 
 app.Use(async (context, next) =>
 {
     await Helper.ConfigureAwaitFalse();
 
-    Match match = Regex.Match(context.Request.Path, "/ws/(?<slotName>.+)", RegexOptions.IgnoreCase | RegexOptions.Compiled | RegexOptions.CultureInvariant);
-    if (!match.Success)
+    if (context.Request.Path != "/ws")
     {
         await next();
         return;
@@ -35,19 +41,13 @@ app.Use(async (context, next) =>
         return;
     }
 
-    string slotName = match.Groups["slotName"].Value;
+    string slotName = context.WebSockets.WebSocketRequestedProtocols[0];
     CurrentGameStates currentGameStates = context.RequestServices.GetRequiredService<CurrentGameStates>();
     Game.State? prevState = currentGameStates.Get(slotName);
     using WebSocket ws = await context.WebSockets.AcceptWebSocketAsync(new WebSocketAcceptContext { DangerousEnableCompression = true });
     byte[] buf = new byte[1];
     while (true)
     {
-        WebSocketReceiveResult recv = await ws.ReceiveAsync(buf, context.RequestAborted);
-        if (recv.MessageType == WebSocketMessageType.Close)
-        {
-            break;
-        }
-
         if (prevState is null)
         {
             TaskCompletionSource<Game.State> nextStateTcs = new(TaskCreationOptions.RunContinuationsAsynchronously);
@@ -73,7 +73,52 @@ app.Use(async (context, next) =>
         Game.State.Proxy proxy = prevState.ToProxy();
         prevState = null;
         await ws.SendAsync(JsonSerializer.SerializeToUtf8Bytes(proxy, Game.State.Proxy.SerializerOptions), WebSocketMessageType.Text, true, CancellationToken.None);
+        WebSocketReceiveResult recv = await ws.ReceiveAsync(buf, context.RequestAborted);
+        if (recv.MessageType == WebSocketMessageType.Close)
+        {
+            break;
+        }
     }
 });
 
 app.Run();
+
+public class Home : ControllerBase
+{
+    [HttpGet("/state/{slotName}")]
+    public FileResult Get(string slotName)
+    {
+        return File(Encoding.UTF8.GetBytes($$"""
+<html>
+    <head>
+        <script type="text/javascript">
+            window.addEventListener('DOMContentLoaded', async function() {
+                const webSocket = new WebSocket(`ws://${window.location.host}/ws`, `{{slotName.Replace("`", "\\`")}}`);
+                webSocket.onmessage = event => {
+                    const dser = JSON.parse(event.data);
+                    document.getElementById('curr').innerHTML = `
+                    <ul>
+                        <li>Current location: ${dser.current_location}</li>
+                        <li>Target location:  ${dser.target_location}</li>
+                    </ul>
+                    Received Items
+                    <ul>
+                        ${dser.received_items.map(item => `<li>${item}</li>`).join('')}
+                    </ul>
+                    Checked Locations
+                    <ul>
+                        ${dser.checked_locations.map(loc => `<li>${loc}</li>`).join('')}
+                    </ul>
+                    `;
+                    webSocket.send('1');
+                };
+            })
+        </script>
+    </head>
+    <body>
+        <div id="curr" />
+    </body>
+</html>
+"""), MediaTypeNames.Text.Html);
+    }
+}
