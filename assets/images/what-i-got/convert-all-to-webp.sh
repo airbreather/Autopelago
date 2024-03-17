17b88afbbbde5a6116da4b519454c634c400c8f2
Joe Amenta <airbreather@linux.com>
2024-03-17 16:50:14 -0400

    I have some more stuff to add here.

--- assets/images/what-i-got/convert-all-to-webp.sh
+++ assets/images/what-i-got/convert-all-to-webp.sh
@@ -2,6 +2,8 @@
 
 # https://stackoverflow.com/a/35374073/1083771
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+mkdir "$SCRIPT_DIR/../Rats"
+mkdir "$SCRIPT_DIR/../Locations"
 
 for f in $SCRIPT_DIR/*.png
 do
@@ -10,9 +12,16 @@ do
     cwebp -mt -z 9 -o "$SCRIPT_DIR/../$fn_webp" "$f"
 done
 
-for f in $SCRIPT_DIR/*.gif
+for f in $SCRIPT_DIR/Rats/*.png
+do
+    fn=${f##*/}
+    fn_webp=${fn%.png}.webp
+    cwebp -mt -z 9 -o "$SCRIPT_DIR/../Rats/$fn_webp" "$f"
+done
+
+for f in $SCRIPT_DIR/Locations/*.gif
 do
     fn=${f##*/}
     fn_webp=${fn%.gif}.webp
-    gif2webp -mt -min_size -o "$SCRIPT_DIR/../$fn_webp" "$f"
+    gif2webp -mt -min_size -o "$SCRIPT_DIR/../Locations/$fn_webp" "$f"
 done
