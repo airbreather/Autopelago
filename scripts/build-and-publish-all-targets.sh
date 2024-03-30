f72dac16148e1318205c4374f9e113fc0336292b
Joe Amenta <airbreather@linux.com>
2024-03-30 19:44:26 -0400

    Revert "does this speed up that part maybe?"
    This reverts commit e9d10f2560312283314a2ce548eb443229aad128.

--- scripts/build-and-publish-all-targets.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-dotnet build -c Release --no-restore &
-dotnet publish src/Autopelago/Autopelago.csproj -c Release -r win-x64 -o artifacts/win-x64 &
-dotnet publish src/Autopelago/Autopelago.csproj -c Release -r linux-x64 -o artifacts/linux-x64 &
-
-wait
