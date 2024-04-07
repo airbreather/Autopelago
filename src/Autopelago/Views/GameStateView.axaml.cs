d33e6b278c3d71a924f6afd449a71fad7f9cdce8
Joe Amenta <airbreather@linux.com>
2024-04-07 10:31:30 -0400

    get the top-level layout looking more complete

--- src/Autopelago/Views/GameStateView.axaml.cs
+++ src/Autopelago/Views/GameStateView.axaml.cs
@@ -1,13 +1,27 @@
 using Autopelago.ViewModels;
 
+using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.ReactiveUI;
 
 namespace Autopelago.Views;
 
 public partial class GameStateView : ReactiveUserControl<GameStateViewModel>
 {
+    private readonly GridLength _initialColumn0Width;
+
+    private readonly GridLength _initialColumn2Width;
+
     public GameStateView()
     {
         InitializeComponent();
+        _initialColumn0Width = MainGrid.ColumnDefinitions[0].Width;
+        _initialColumn2Width = MainGrid.ColumnDefinitions[2].Width;
+    }
+
+    private void OnMainGridSplitterDoubleTapped(object? sender, TappedEventArgs e)
+    {
+        MainGrid.ColumnDefinitions[0].Width = _initialColumn0Width;
+        MainGrid.ColumnDefinitions[2].Width = _initialColumn2Width;
     }
 }
