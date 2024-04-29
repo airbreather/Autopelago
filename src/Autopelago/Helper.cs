a126df83aaf8fc90ff74007e1e1c27d87c13a423
skidznet <skidznet@gmail.com>
2024-04-29 13:37:01 -0400

    Update src/Autopelago/AutopelagoDefinitions.yml

--- src/Autopelago/Helper.cs
+++ src/Autopelago/Helper.cs
@@ -1,10 +1,18 @@
 using System.Runtime.CompilerServices;
 
+using Avalonia;
+using Avalonia.Controls;
+
 namespace Autopelago;
 
 public static class Helper
 {
     public static GetOffSyncContextAwaitableAndAwaiter ConfigureAwaitFalse() => default;
+
+    public static IEnumerable<PixelRect> Intersecting(this Screens screens, PixelRect bounds)
+    {
+        return screens.All.Select(s => s.Bounds).Where(b => b.Intersects(bounds));
+    }
 
     public readonly struct GetOffSyncContextAwaitableAndAwaiter : INotifyCompletion
     {
