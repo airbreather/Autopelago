c4a4ec008701f36892d3dd37de9d4da38f46de99
Joe Amenta <airbreather@linux.com>
2025-07-10 20:17:14 -0400

    Revamp connect screen form layout; add new input fields and options; refine global styles.

--- src/app/connect-screen/connect-screen.ts
+++ src/app/connect-screen/connect-screen.ts
@@ -4,40 +4,125 @@ import {Component, computed, linkedSignal, signal, WritableSignal} from '@angula
   selector: 'app-connect-screen',
   standalone: true,
   template: `
-    <form>
-      <div>
-        <label for="slot">Slot
-          <input #slotInput
-                 required
-                 id="slot"
-                 type="text"
-                 [value]="slot()"
-                 (input)="slot.set(slotInput.value)" />
-        </label>
-      </div>
-      <div>
-        <label for="host">Host
-          <input #hostInput
-                 id="host"
-                 type="text"
-                 [value]="host()"
-                 (input)="directHost.set(hostInput.value)" />
-        </label>
-      </div>
-      <div>
-        <label for="port">Port
-          <input #portInput
-                 id="port"
-                 type="number"
-                 min="1"
-                 max="65535"
-                 [value]="port()"
-                 (input)="port.set(portInput.valueAsNumber)" />
-        </label>
-      </div>
-    </form>
+    <div class="inputs">
+      <label for="slot">Slot:</label>
+      <input #slotInput
+             required
+             id="slot"
+             type="text"
+             [value]="slot()"
+             (input)="slot.set(slotInput.value)" />
+      <label for="host">Host:</label>
+      <input #hostInput
+             id="host"
+             type="text"
+             [value]="host()"
+             (input)="directHost.set(hostInput.value)" />
+      <label for="port">Port:</label>
+      <input #portInput
+             id="port"
+             type="number"
+             min="1"
+             max="65535"
+             [value]="port()"
+             (input)="port.set(portInput.valueAsNumber)" />
+      <label for="password">Password:</label>
+      <input #passwordInput
+             id="password"
+             type="password"
+             [value]="password()"
+             (input)="password.set(passwordInput.value)" />
+      <fieldset class="inputs" [style.grid-column]="'span 2'">
+        <legend>Time Between Steps (sec.)</legend>
+        <label for="minTime">Minimum:</label>
+        <input #minTimeInput
+               id="minTime"
+               type="number"
+               [value]="minTime()"
+               (input)="minTime.set(minTimeInput.valueAsNumber)" />
+        <label for="maxTime">Maximum:</label>
+        <input #maxTimeInput
+               id="maxTime"
+               type="number"
+               [value]="maxTime()"
+               (input)="maxTime.set(maxTimeInput.valueAsNumber)" />
+      </fieldset>
+    </div>
+    <div class="inputs">
+      <input #enableTileAnimationsInput
+             id="enableTileAnimations"
+             type="checkbox"
+             [checked]="enableTileAnimations()"
+             (input)="enableTileAnimations.set(enableTileAnimationsInput.checked)" />
+      <label for="enableTileAnimations">Enable tile animations</label>
+
+      <input #enableRatAnimationsInput
+             id="enableRatAnimations"
+             type="checkbox"
+             [checked]="enableRatAnimations()"
+             (input)="enableRatAnimations.set(enableRatAnimationsInput.checked)" />
+      <label for="enableRatAnimations">Enable rat animations</label>
+
+      <input #sendChatMessagesInput
+             id="sendChatMessages"
+             type="checkbox"
+             [checked]="sendChatMessages()"
+             (input)="sendChatMessages.set(sendChatMessagesInput.checked)" />
+      <label for="sendChatMessages">Send chat messages...</label>
+    </div>
+    <div class="inputs" [style.padding-left]="'20px'">
+      <input #whenTargetChangesInput
+             id="whenTargetChanges"
+             type="checkbox"
+             [disabled]="!sendChatMessages()"
+             [checked]="whenTargetChanges()"
+             (input)="whenTargetChanges.set(whenTargetChangesInput.checked)" />
+      <label for="whenTargetChanges">when target changes</label>
+
+      <input #whenBecomingBlockedInput
+             id="whenBecomingBlocked"
+             type="checkbox"
+             [disabled]="!sendChatMessages()"
+             [checked]="whenBecomingBlocked()"
+             (input)="whenBecomingBlocked.set(whenBecomingBlockedInput.checked)" />
+      <label for="whenBecomingBlocked">when becoming blocked</label>
+
+      <input #whenStillBlockedInput
+             id="whenStillBlocked"
+             type="checkbox"
+             [disabled]="!(sendChatMessages() && whenBecomingBlocked())"
+             [checked]="whenStillBlocked()"
+             (input)="whenStillBlocked.set(whenStillBlockedInput.checked)" />
+      <label for="whenStillBlocked">when STILL blocked</label>
+
+      <input #whenBecomingUnblockedInput
+             id="whenBecomingUnblocked"
+             type="checkbox"
+             [disabled]="!sendChatMessages()"
+             [checked]="whenBecomingUnblocked()"
+             (input)="whenBecomingUnblocked.set(whenBecomingUnblockedInput.checked)" />
+      <label for="whenBecomingUnblocked">when becoming unblocked</label>
+
+      <input #forOneTimeEventsInput
+             id="forOneTimeEvents"
+             type="checkbox"
+             [disabled]="!sendChatMessages()"
+             [checked]="forOneTimeEvents()"
+             (input)="forOneTimeEvents.set(forOneTimeEventsInput.checked)" />
+      <label for="forOneTimeEvents">for one-time events</label>
+    </div>
   `,
   styles: `
+    div:not(:first-child) {
+      padding-top: 5px;
+    }
+    
+    .inputs {
+      display: grid;
+      grid-template-columns: max-content 1fr;
+      grid-gap: 5px;
+    }
+    
     input:invalid {
       border: 2px dashed red;
     }
