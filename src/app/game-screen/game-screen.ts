7f6b2a3907c7111ade917ea46dbd86d8d34c4a18
Joe Amenta <airbreather@linux.com>
2025-07-13 11:15:34 -0400

    Enhance `GameScreen` component with resizable split-view layout improvements, dynamic map integration, and updated styles. Add SVG map asset.

--- src/app/game-screen/game-screen.ts
+++ src/app/game-screen/game-screen.ts
@@ -1,8 +1,12 @@
-import { Component, inject, viewChild } from '@angular/core';
+import { Component, computed, inject, viewChild } from '@angular/core';
+import { rxResource, takeUntilDestroyed, toObservable } from "@angular/core/rxjs-interop";
+
 import { SplitAreaComponent, SplitComponent } from "angular-split";
-import { takeUntilDestroyed, toObservable } from "@angular/core/rxjs-interop";
+
+import { map, mergeMap } from "rxjs/operators";
+
 import { GameScreenStore } from "../store/game-screen-store";
-import { mergeMap } from "rxjs/operators";
+import { ActivatedRoute, UrlSegment } from "@angular/router";
 
 @Component({
   selector: 'app-game-screen',
@@ -10,12 +14,31 @@ import { mergeMap } from "rxjs/operators";
     SplitComponent, SplitAreaComponent,
   ],
   template: `
-    <as-split class="outer" unit="percent" direction="horizontal" gutterDblClickDuration="500" (gutterDblClick)="onGutterDblClick()" #x>
-      <as-split-area [size]="leftSize()" [minSize]="5" [maxSize]="95">
-        <p>!</p>
+    <as-split #outer class="outer" unit="percent" direction="horizontal"
+              gutterDblClickDuration="500" (gutterDblClick)="onGutterDblClick()">
+      <as-split-area class="left" [size]="leftSize()" [minSize]="5" [maxSize]="95">
+        <div class="top">
+          Just the name
+        </div>
+        <div class="bottom">
+          All the items!
+        </div>
       </as-split-area>
-      <as-split-area>
-        <p>?</p>
+      <as-split-area class="right">
+        <div class="top">
+          <!--suppress AngularNgOptimizedImage -->
+          <img alt="map" [src]="mapSrc()" />
+        </div>
+        <div class="bottom">
+          <div class="tab-map">
+            Map
+          </div>
+          <div class="tab-arcade">
+            Arcade
+          </div>
+          <div class="tab-filler">
+          </div>
+        </div>
       </as-split-area>
     </as-split>
   `,
@@ -24,11 +47,62 @@ import { mergeMap } from "rxjs/operators";
       width: 100vw;
       height: 100vh;
     }
+
+    .left {
+      display: flex;
+      flex-direction: column;
+
+      .top {
+        flex: 0;
+      }
+
+      .bottom {
+        flex: 1;
+      }
+    }
+
+    .right {
+      display: flex;
+      flex-direction: column;
+      height: 100%;
+
+      .top {
+        flex: 1;
+        overflow: auto;
+      }
+
+      .bottom {
+        flex: 0;
+        display: flex;
+
+        .tab-map {
+          flex: 0;
+        }
+
+        .tab-arcade {
+          flex: 0;
+        }
+
+        .tab-filler {
+          flex: 1;
+        }
+      }
+    }
   `
 })
 export class GameScreen {
   readonly #store = inject(GameScreenStore);
-  protected readonly splitRef = viewChild.required<SplitComponent>('x');
+  readonly #route = inject(ActivatedRoute);
+  protected readonly splitRef = viewChild.required<SplitComponent>('outer');
+
+  pathBase = rxResource<UrlSegment | null, never>({
+    defaultValue: null,
+    stream: () => this.#route.root.url.pipe(map(u => u[0])),
+  });
+  mapSrc = computed(() => {
+    const pathBase = this.pathBase.value();
+    return pathBase ? pathBase.path + '/assets/map.svg' : null;
+  });
 
   leftSize = this.#store.leftSize;
 
