a7cd259b4ed3c399c3283c45801edc363975d146
Joe Amenta <airbreather@linux.com>
2025-07-13 15:30:33 -0400

    Extract `LandmarkMarkers` component from `GameTabMap` to improve modularity and maintainability; move marker rendering logic and styles into the new component.

--- src/app/game-screen/game-tabs/game-tab-map/game-tab-map.ts
+++ src/app/game-screen/game-tabs/game-tab-map/game-tab-map.ts
@@ -3,30 +3,18 @@ import { ActivatedRoute, UrlSegment } from "@angular/router";
 
 import { rxResource } from "@angular/core/rxjs-interop";
 import { map } from "rxjs";
-
-import { Landmark, LANDMARKS } from "../../../data/locations";
+import { LandmarkMarkers } from "./landmark-markers/landmark-markers";
 
 @Component({
   selector: 'app-game-tab-map',
-  imports: [],
+  imports: [
+    LandmarkMarkers
+  ],
   template: `
     <div class="outer">
       <!--suppress AngularNgOptimizedImage -->
       <img alt="map" [src]="mapSrc()" />
-      @for (landmarks of landmarks(); track landmarks.key) {
-        <img class="quest-marker"
-             [alt]="landmarks.key"
-             [src]="qUrls()[(landmarks.value.sprite_index + 1) % 2]"
-             [style.left]="'calc(100% * ' + landmarks.calcQLeft + ' / 300)'"
-             [style.top]="'calc(100% * ' + landmarks.calcQTop + ' / 450)'" />
-
-        <img class="landmark"
-             [class.unchecked]="landmarks.value.sprite_index % 2 === 0" 
-             [alt]="landmarks.key"
-             [src]="landmarks.url"
-             [style.left]="'calc(100% * ' + landmarks.calcLeft + ' / 300)'"
-             [style.top]="'calc(100% * ' + landmarks.calcTop + ' / 450)'" />
-      }
+      <app-landmark-markers [pathBase]="pathBase.value()?.path ?? null" />
     </div>
   `,
   styles: `
@@ -34,26 +22,6 @@ import { Landmark, LANDMARKS } from "../../../data/locations";
       position: relative;
       pointer-events: none;
       user-select: none;
-    }
-
-    .quest-marker {
-      position: absolute;
-      width: calc(100% * 12 / 300);
-      height: calc(100% * 12 / 450);
-    }
-
-    .landmark {
-      position: absolute;
-      width: calc(100% * 16 / 300);
-      height: calc(100% * 16 / 450);
-
-      &.unchecked {
-        filter: drop-shadow(8px 16px 16px black) saturate(10%) brightness(40%);
-      }
-
-      &:not(.unchecked) {
-        filter: drop-shadow(8px 16px 16px black);
-      }
     }
   `,
 })
@@ -66,25 +34,5 @@ export class GameTabMap {
   mapSrc = computed(() => {
     const pathBase = this.pathBase.value();
     return pathBase ? pathBase.path + '/assets/images/map.svg' : null;
-  });
-
-  readonly landmarks = computed(() => {
-    const pathBase = this.pathBase.value();
-    const pathBase2 = pathBase ? pathBase.path + '/assets/images/locations/' : '';
-    return Object.entries(LANDMARKS).map(([k, v]) => ({
-      key: k,
-      value: v as Landmark,
-      url: pathBase2 + k + '.webp',
-      calcLeft: (v.coords[0] - 8).toString(),
-      calcTop: (v.coords[1] - 8).toString(),
-      calcQLeft: (v.coords[0] - 6).toString(),
-      calcQTop: (v.coords[1] - 21).toString(),
-    }));
-  });
-
-  readonly qUrls = computed(() =>  {
-    const pathBase = this.pathBase.value();
-    const pathBase2 = pathBase ? pathBase.path + '/assets/images/locations/' : '';
-    return [pathBase2 + 'yellow_quest.webp', pathBase2 + 'gray_quest.webp'];
   });
 }
---
+++ src/app/game-screen/game-tabs/game-tab-map/landmark-markers/landmark-markers.ts
@@ -0,0 +1,66 @@
+import { Component, computed, input } from '@angular/core';
+
+import { Landmark, LANDMARKS } from "../../../../data/locations";
+
+@Component({
+  selector: 'app-landmark-markers',
+  imports: [],
+  template: `
+    @for (landmarks of landmarks(); track landmarks.key) {
+      <img class="quest-marker"
+           [alt]="landmarks.key"
+           [src]="qUrls()[(landmarks.value.sprite_index + 1) % 2]"
+           [style.left]="'calc(100% * ' + landmarks.calcQLeft + ' / 300)'"
+           [style.top]="'calc(100% * ' + landmarks.calcQTop + ' / 450)'" />
+
+      <img class="landmark"
+           [class.unchecked]="landmarks.value.sprite_index % 2 === 0"
+           [alt]="landmarks.key"
+           [src]="landmarks.url"
+           [style.left]="'calc(100% * ' + landmarks.calcLeft + ' / 300)'"
+           [style.top]="'calc(100% * ' + landmarks.calcTop + ' / 450)'" />
+    }
+  `,
+  styles: `
+    .quest-marker {
+      position: absolute;
+      width: calc(100% * 12 / 300);
+      height: calc(100% * 12 / 450);
+    }
+
+    .landmark {
+      position: absolute;
+      width: calc(100% * 16 / 300);
+      height: calc(100% * 16 / 450);
+
+      &.unchecked {
+        filter: drop-shadow(8px 16px 16px black) saturate(10%) brightness(40%);
+      }
+
+      &:not(.unchecked) {
+        filter: drop-shadow(8px 16px 16px black);
+      }
+    }
+  `,
+})
+export class LandmarkMarkers {
+  readonly pathBase = input.required<string | null>();
+
+  readonly landmarks = computed(() => {
+    const pathBase = (this.pathBase() ?? '') + '/assets/images/locations/';
+    return Object.entries(LANDMARKS).map(([k, v]) => ({
+      key: k,
+      value: v as Landmark,
+      url: pathBase + k + '.webp',
+      calcLeft: (v.coords[0] - 8).toString(),
+      calcTop: (v.coords[1] - 8).toString(),
+      calcQLeft: (v.coords[0] - 6).toString(),
+      calcQTop: (v.coords[1] - 21).toString(),
+    }));
+  });
+
+  readonly qUrls = computed(() =>  {
+    const pathBase = (this.pathBase() ?? '') + '/assets/images/locations/';
+    return [pathBase + 'yellow_quest.webp', pathBase + 'gray_quest.webp'];
+  });
+}
