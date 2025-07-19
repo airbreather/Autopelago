f0fda8ec56b2d175d038e5e9d1f9a1863234c189
Joe Amenta <airbreather@linux.com>
2025-07-19 12:11:12 -0400

    Add `ProgressionItemStatus` component to display progression items with dynamic styling; integrate item data and visuals.

--- src/app/game-screen/status-display/progression-item-status/progression-item-status.ts
+++ src/app/game-screen/status-display/progression-item-status/progression-item-status.ts
@@ -1,15 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { PROGRESSION_ITEMS_TO_DISPLAY } from '../../../data/items';
+import { NgClass } from '@angular/common';
+
+function createItem(name: string, index: number) {
+  const collected = signal(index % 4 === 0);
+  return {
+    name,
+    collected,
+    offsetX: computed(() => collected() ? 0 : 65),
+    offsetY: index * 65,
+  };
+}
 
 @Component({
   selector: 'app-progression-item-status',
-  imports: [],
+  imports: [
+    NgClass,
+  ],
   template: `
-    <div>
-      ALL THE ITEMS!!!
+    <div class="outer">
+      @for (item of items; track item.name) {
+        <div class="item-container" [ngClass]="{ collected: item.collected() }">
+          <img class="item"
+               src="/assets/images/items.webp"
+               [alt]="item.name"
+               [style.object-position]="-item.offsetX() + 'px ' + -item.offsetY + 'px'">
+        </div>
+      }
     </div>
   `,
-  styles: '',
+  styles: `
+    .outer {
+      display: flex;
+      flex-wrap: wrap;
+    }
+
+    .item-container {
+      margin: 5px;
+      padding: 5px;
+
+      border: 2px solid black;
+      border-radius: 8px;
+
+      &.collected {
+        border: 2px solid #FFCE00;
+      }
+    }
+
+    .item {
+      object-fit: none;
+      width: 64px;
+      height: 64px;
+    }
+  `,
 })
-// eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export class ProgressionItemStatus {
+  readonly items = PROGRESSION_ITEMS_TO_DISPLAY.map(createItem);
 }
