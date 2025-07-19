9ae6044d537fc956c9e9c5fa00591a8c3af2a6f8
Joe Amenta <airbreather@linux.com>
2025-07-19 16:05:44 -0400

    Enhance `PlayerNameAndNavigation` component: integrate dynamic player name display; add responsive text resizing for player name and navigation button; refine styles and layout.

--- src/app/game-screen/status-display/player-name-and-navigation/player-name-and-navigation.ts
+++ src/app/game-screen/status-display/player-name-and-navigation/player-name-and-navigation.ts
@@ -1,6 +1,41 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  DestroyRef,
+  effect,
+  ElementRef,
+  inject,
+  Injector,
+  Signal,
+  viewChild,
+} from '@angular/core';
 
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { RouterLink } from '@angular/router';
+
+import { Subscription } from 'rxjs';
+
+import { ConnectScreenStore } from '../../../store/connect-screen.store';
+import { resizeEvents } from '../../../util';
+
+function fitTextToContainer(inner: HTMLElement, outer: HTMLElement, max: number) {
+  let fontSize = window.getComputedStyle(inner).fontSize;
+
+  while (inner.scrollWidth <= outer.clientWidth) {
+    const fontSizeNum = Math.min(max, Number(/^\d+/.exec(fontSize)) + 5);
+    if (fontSizeNum >= max) {
+      break;
+    }
+
+    fontSize = fontSize.replace(/^\d+/, fontSizeNum.toString());
+    inner.style.fontSize = fontSize;
+  }
+
+  while (inner.scrollWidth > outer.clientWidth) {
+    fontSize = fontSize.replace(/^\d+/, s => (Number(s) - 1).toString());
+    inner.style.fontSize = fontSize;
+  }
+}
 
 @Component({
   selector: 'app-player-name-and-navigation',
@@ -8,13 +43,55 @@ import { RouterLink } from '@angular/router';
     RouterLink,
   ],
   template: `
-    <div>
-      <div>PLAYER NAME</div>
-      <div><button routerLink="/">Back to Main Menu</button></div>
+    <div #outer class="outer">
+      <div #playerNameDiv class="player-name">{{ playerName() }}</div>
+      <div><button #returnButton class="return-button" routerLink="/">Back to Main Menu</button></div>
     </div>
   `,
-  styles: '',
+  styles: `
+    .outer {
+      text-align: left;
+      margin-left: 5px;
+      margin-right: 5px;
+    }
+
+    .player-name {
+      font-size: 50px;
+    }
+
+    .return-button {
+      font-size: 30px;
+      text-wrap: nowrap;
+    }
+  `,
 })
-// eslint-disable-next-line @typescript-eslint/no-extraneous-class
-export class PlayerNameAndNavigation {
+export class PlayerNameAndNavigation implements AfterViewInit {
+  readonly #connectScreenStore = inject(ConnectScreenStore);
+  readonly #destroy = inject(DestroyRef);
+  readonly #injector = inject(Injector);
+
+  readonly playerName = this.#connectScreenStore.slot;
+
+  readonly outerElement = viewChild.required<ElementRef<HTMLElement>>('outer');
+  readonly playerNameElement = viewChild.required<ElementRef<HTMLElement>>('playerNameDiv');
+  readonly returnButtonElement = viewChild.required<ElementRef<HTMLElement>>('returnButton');
+
+  ngAfterViewInit(): void {
+    this.#resizeText(this.playerNameElement, 50);
+    this.#resizeText(this.returnButtonElement, 30);
+  }
+
+  #resizeText(innerRef: Signal<ElementRef<HTMLElement>>, max: number) {
+    let prevSub = new Subscription();
+    effect(() => {
+      prevSub.unsubscribe();
+      const outer = this.outerElement().nativeElement;
+      const inner = innerRef().nativeElement;
+      prevSub = resizeEvents(outer)
+        .pipe(takeUntilDestroyed(this.#destroy))
+        .subscribe(() => {
+          fitTextToContainer(inner, outer, max);
+        });
+    }, { injector: this.#injector });
+  }
 }
