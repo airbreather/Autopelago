730e0da28346e01a2eb90267f2c09ceea5c84929
Joe Amenta <airbreather@linux.com>
2025-10-21 18:29:48 -0400

    start custom builder

---
+++ src/bake-data/index.ts
@@ -0,0 +1,18 @@
+﻿import { type BuilderOutput, createBuilder } from '@angular-devkit/architect';
+import { type JsonObject } from '@angular-devkit/core';
+
+import { readFile } from 'node:fs/promises';
+
+interface Options extends JsonObject {
+  'defs-file': string;
+}
+
+export default createBuilder<Options>(async (options) => {
+  const data = await readFile(options['defs-file'], { encoding: 'utf8' });
+  return new Promise<BuilderOutput>((resolve) => {
+    resolve({
+      success: false,
+      error: `This is where I would bake the data from ${options['defs-file']}.`,
+    });
+  });
+});
