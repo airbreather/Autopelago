c375d93144f5f6ad40582a857b4c38ed23abc644
Joe Amenta <airbreather@linux.com>
2025-11-28 14:11:40 -0500

    try this color (and also start tagging version numbers)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
+
+import version from '../version.json';
 
 @Component({
   selector: 'app-root',
@@ -13,4 +15,7 @@ import { RouterOutlet } from '@angular/router';
 })
 // eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export class App {
+  constructor() {
+    inject(Title).setTitle(`Autopelago ${version} | A Game So Easy, It Plays Itself!`);
+  }
 }
